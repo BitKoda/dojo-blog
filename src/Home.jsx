cdcd3331e8e009e28e4164ffaa705d3f15a04e40
Jamie Finlay <jpfinlay@gmail.com>
2022-04-11 15:35:58 +0100

    Create delete button and pass handleClick function as a prop from home component to child component

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,12 +6,19 @@ const Home = () => {
    const [blogs, setBlogs] = useState([
       { title: 'Hacking Websites', body: 'On hacking websites with JavaScript..', author: 'Jamo', id: 1 },
       { title: 'A Beer Site', body: 'On the lots of different beers you can now buy online..', author: 'Deno', id: 2 },
-      { title: 'A Rum Site', body: 'A rum review website for officionados..', author: 'Big-Z', id: 3 }
+      { title: 'Monetising your JavaScript Websites', body: 'In this article, we talk about how to make money from your JavaScript..', author: 'Jamo', id: 3 },
+      { title: 'A Rum Site', body: 'A rum review website for officionados..', author: 'Big-Z', id: 4 }
    ]);
+
+   const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id);
+      setBlogs(newBlogs)
+   }
 
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="All Blogs" />
+           <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+          
         </div>
      );
 }
