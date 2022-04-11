35e7b1bcc65059aa7180738496bd15f768afabb3
Jamie Finlay <jpfinlay@gmail.com>
2022-04-11 17:38:17 +0100

    Add error handling and loading functionality to app

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,22 +2,32 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
-   
    const [blogs, setBlogs] = useState(null);
+   const [isLoading, setIsLoading] = useState(true);
+   const [error, setError] = useState(null);
 
    useEffect(() => {
-      fetch('http://localhost:8000/blogs')
+      fetch('http://localhost:8000/blogz')
       .then(res => {
+         if (!res.ok) throw Error('Sorry, there was a problem fetching data for that resource');
          return res.json();
       })
       .then((data) => {
          setBlogs(data);
+         setIsLoading(false);
+         setError(null);
+      })
+      .catch(err => {
+         setIsLoading(false);
+         setError(err.message);
       })
    }, [])
 
    return ( 
       <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      { error && <div> { error } </div> }
+      { isLoading && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
       </div>
      );
 }
