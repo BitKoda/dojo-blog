f77c09e04239ae11251730c78f39ab500768446f
Jamie Finlay <jpfinlay@gmail.com>
2022-04-11 16:07:44 +0100

    Use json-server to deliver blog items, and remove delete button for each blog post

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,25 +1,24 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
    
-   const [blogs, setBlogs] = useState([
-      { title: 'Hacking Websites', body: 'On hacking websites with JavaScript..', author: 'Jamo', id: 1 },
-      { title: 'A Beer Site', body: 'On the lots of different beers you can now buy online..', author: 'Deno', id: 2 },
-      { title: 'Monetising your JavaScript Websites', body: 'In this article, we talk about how to make money from your JavaScript..', author: 'Jamo', id: 3 },
-      { title: 'A Rum Site', body: 'A rum review website for officionados..', author: 'Big-Z', id: 4 }
-   ]);
+   const [blogs, setBlogs] = useState(null);
 
-   const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !== id);
-      setBlogs(newBlogs)
-   }
+   useEffect(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+         return res.json();
+      })
+      .then((data) => {
+         setBlogs(data);
+      })
+   }, [])
 
-    return ( 
-        <div className="home">
-           <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-          
-        </div>
+   return ( 
+      <div className="home">
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      </div>
      );
 }
  
